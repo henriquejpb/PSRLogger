2c2c701719072c497ee0296513e0549c817e5d18
Henrique Barcelos <rick.hjpbarcelos@gmail.com>
2013-11-01 20:42:59 -0200

    Fixes Appliction\Service\User to fit PSR-1

--- src/Application/Service/User.php
+++ src/Application/Service/User.php
@@ -3,12 +3,34 @@ namespace Application\Service;
 
 use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
+use Application\Entity\User as UserEntity;
 
-class User {
+class User 
+{
     use LoggerAwareTrait;
 
-    public function __construct(LoggerInterface $logger) {
+    private $userStorage = array();
+
+    public function __construct(LoggerInterface $logger) 
+    {
         $this->setLogger($logger);
         $this->getLogger()->info("Started user service");
+    }
+
+    public function addUser(UserEntity $user) 
+    {
+        $this->getLogger()->info("Added user {0}", array($user->getName()));
+        $this->userStorage[] = $user;
+    }
+
+    public function removeUser(UserEntity $user) 
+    {
+        foreach ($this->userStorage as $key => $stored) {
+            if ($stored === $user) {
+                $this->getLogger()->info("Removed user {0}", array($user->getName()));
+                unset($this->userStorage[$key]);
+                $this->userStorage = array_values($this->userStorage);
+            }
+        }
     }
 }
