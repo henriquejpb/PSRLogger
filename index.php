6741d81102872305a4a09f4a71603b06dbb7dfc0
Henrique Barcelos <rick.hjpbarcelos@gmail.com>
2013-11-01 20:44:34 -0200

    Adds application User entity

--- index.php
+++ index.php
@@ -3,15 +3,15 @@ require_once 'src/autoload.php';
 
 use Psr\Log\FileLogger;
 use Application\Service\User as UserService;
+use Application\Entity\User;
 
 $logger = new FileLogger("log.log");
 $service = new UserService($logger);
 
-/* $logger->info("This is an info"); */
-/* $logger->warning("This is a warning"); */
-/* $logger->notice("This is a notice"); */
-/* $logger->debug("This is a debug note"); */
-/* $logger->emergency("This is an emergency"); */
-/* $logger->critical("This is a critical error"); */
-/* $logger->error(new Exception("foo")); */
+$userHenrique = new User('Henrique');
+$userJose = new User('José');
+
+$service->addUser($userHenrique);
+$service->addUser($userJose);
+$service->removeUser($userJose);
 
